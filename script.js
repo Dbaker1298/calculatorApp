94f09ebb56f5368377d509c5b70e5a35fcd13273
David Baker <dpbaker1298@gmail.com>
2019-03-14 00:10:39 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let runningTotal = 0;
+let buffer = '0';
+let previousOperator;
+const screen = document.querySelector('.answer');
+
+
+document.querySelector('.btn').addEventListener('click', function (event) {
+  buttonClick(event.target.value);
+})
+
+function buttonClick(value) {
+  if (isNaN(parseInt(value))) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+}
+
+function handleNumber(value) {
+  if (buffer === '0') {
+    buffer = value;
+  } else {
+    buffer += value;
+  }
+  rerender();
+}
+
+function handleSymbol(value) {
+
+}
+
+function rerender() {
+  screen.innerText = buffer;
+}
